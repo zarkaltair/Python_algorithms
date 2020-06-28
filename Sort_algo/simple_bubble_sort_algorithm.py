8fac7ec85018713066a726271054ec977e10e2fb
Zark_Altair <zarkaltair@gmail.com>
2020-06-28 15:44:46 +0500

    change tabs to spaces

--- Sort_algo/simple_bubble_sort_algorithm.py
+++ Sort_algo/simple_bubble_sort_algorithm.py
@@ -1,11 +1,11 @@
 # Bubble sort
 def bubble_sort(arr):
-	for nums in range(len(arr)-1, 0, -1):
-		for x in range(nums):
-			if arr[x] > arr[x + 1]:
-				temp_num = arr[x]
-				arr[x] = arr[x + 1]
-				arr[x + 1] = temp_num
+    for nums in range(len(arr)-1, 0, -1):
+        for x in range(nums):
+            if arr[x] > arr[x + 1]:
+                temp_num = arr[x]
+                arr[x] = arr[x + 1]
+                arr[x + 1] = temp_num
 
 sort_me = [15, 9, 1, 15, 68, 1, 22, 3, 0, 5]
 
